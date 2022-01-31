d26d77dd8973781e2a6213d5a1fdf4259a8afd59
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-01-31 14:08:04 +0600

    f to c

---
+++ farenhight-celcious.js
@@ -0,0 +1,11 @@
+function farenhightToCelcious(num1,num2,num3)
+ {
+var total = (num1-num2)*num3;
+
+return total;
+ } 
+ var firstNum = 100;
+ var secondNum = 32;
+ var thirdNum=5/9;
+ var result = farenhightToCelcious(firstNum,secondNum,thirdNum);
+ console.log('result value', result);
