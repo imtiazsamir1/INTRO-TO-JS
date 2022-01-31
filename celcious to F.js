210900b6f9ef6e0393107b87ecb10ca9b239adf2
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-01-31 13:34:58 +0600

    c to f

---
+++ celcious to F.js
@@ -0,0 +1,11 @@
+function celciousToFarenhight(num1,num2,num3)
+ {
+var total = num1*num2+num3;
+
+return total;
+ } 
+ var firstNum = -3;
+ var secondNum = 9/5;
+ var thirdNum=32;
+ var result = celciousToFarenhight(firstNum,secondNum,thirdNum);
+ console.log('result value', result);
--- first.js
+++ first.js
@@ -1,26 +1,25 @@
-// function addTwoNumbers(num1,num2)
-// {
-// var total = num1+num2;
-// return total;
-// }
-// var firstNum = 34;
-// var secondNum = 2;
-// var result = addTwoNumbers(firstNum,secondNum);
-// console.log('result value', result);
+ function celciousToFarenhight(num1,num2,num3)
+ {
+var total = num1*num2+num3; return total;
+ } var firstNum = -3;
+ var secondNum = 9/5;
+ var thirdNum=32;
+ var result = celciousToFarenhight(firstNum,secondNum,thirdNum);
+ console.log('result value', result);
 
- function twoMultiNum(num1,i){
-      var total = num1* i;
-     return total;
- }
-     var fastNum = 2;
-     for(
-             i=1;i<=10;i++
-         )
+//  function twoMultiNum(num1,i){
+//       var total = num1* i;
+//      return total;
+//  }
+//      var fastNum = 2;
+//      for(
+//              i=1;i<=10;i++
+//          )
         
 
-     var  secondNum = i;
-    var result = twoMultiNum(fastNum,secondNum);
-     console.log('result value',result);
+//      var  secondNum = i;
+//     var result = twoMultiNum(fastNum,secondNum);
+//      console.log('result value',result);
 // var i=1;
 // while(i<=10){
 //     i=1;
