906784b5be5dceb3c55542a90dc3df89123e01b7
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-01-31 17:29:14 +0600

    simple-interest

---
+++ simple-interest.js
@@ -0,0 +1,13 @@
+// simple interest
+// i=Prt
+function interestAmount(num1,num2,num3)
+ {
+var total = num1*num2*num3;
+
+return total;
+ } 
+ var firstNum = 4500.00;
+ var secondNum = 0.095;
+ var thirdNum=7;
+ var result = interestAmount(firstNum,secondNum,thirdNum);
+ console.log('Interest Amount:', result);
